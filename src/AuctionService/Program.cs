7e540bff4b0f2f91d92d615b698b0167974bb912
Fernando D'luccas <dluccash@hotmail.com>
2023-09-06 19:03:23 -0300

    feat: Shape data for return

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -10,6 +10,9 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
 {
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
