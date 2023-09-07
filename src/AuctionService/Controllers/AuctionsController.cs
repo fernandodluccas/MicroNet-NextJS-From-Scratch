4df9b48818af2b996a7dbd58bbf140cc5188616b
Fernando D'luccas <dluccash@hotmail.com>
2023-09-06 23:21:03 -0300

    ´me dá um commit pra

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,23 @@
+﻿using AuctionService.Data;
+using AuctionService.DTOs;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionService.Controllers;
+
+[ApiController]
+[Route("api/auctions")]
+public class AuctionsController : ControllerBase
+{
+    private readonly AuctionDbContext _context;
+    private readonly IMapper _mapper;
+
+    public AuctionsController(AuctionDbContext context, IMapper mapper)
+    {
+        this._context = context;
+        this._mapper = mapper;
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<<List<AuctionDto>>>
+}
---
+++ src/AuctionService/Data/Migrations/20230907020110_AddAuctionEndField.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuctionEndField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Item_Auctions_AuctionId",
+                table: "Item");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Item",
+                table: "Item");
+
+            migrationBuilder.RenameTable(
+                name: "Item",
+                newName: "Items");
+
+            migrationBuilder.RenameColumn(
+                name: "ReserevePrice",
+                table: "Auctions",
+                newName: "ReservePrice");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Item_AuctionId",
+                table: "Items",
+                newName: "IX_Items_AuctionId");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AuctionEnd",
+                table: "Auctions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Items",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Items",
+                table: "Items",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Auctions_AuctionId",
+                table: "Items",
+                column: "AuctionId",
+                principalTable: "Auctions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Auctions_AuctionId",
+                table: "Items");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Items",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "AuctionEnd",
+                table: "Auctions");
+
+            migrationBuilder.RenameTable(
+                name: "Items",
+                newName: "Item");
+
+            migrationBuilder.RenameColumn(
+                name: "ReservePrice",
+                table: "Auctions",
+                newName: "ReserevePrice");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Items_AuctionId",
+                table: "Item",
+                newName: "IX_Item_AuctionId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ImageUrl",
+                table: "Item",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Item",
+                table: "Item",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Item_Auctions_AuctionId",
+                table: "Item",
+                column: "AuctionId",
+                principalTable: "Auctions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
